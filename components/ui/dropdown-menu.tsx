f2a5b6615ceecc2aff7b09a117b9fdf0f38d9a79
Iorhen Tomna <iorhentomna@gmail.com>
2026-09-13 18:49:59 +0100

    fix: failing build

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
+
 interface DropdownMenuContextValue {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
